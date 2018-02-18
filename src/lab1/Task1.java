a13270cb5c7c1e6156a0dc3fa4285ed9291ed145
MisterRnobe <medvedev.nikita97@yandex.ru>
2018-02-18 14:46:31 +0300

    Task is almost complete

--- src/lab1/Task1.java
+++ src/lab1/Task1.java
@@ -2,23 +2,27 @@ package lab1;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Task1 {
-    public static void main(String[] args){
-        SimpleDateFormat date = new SimpleDateFormat("YYYY-MM-DD'T'HH:mm:ssZ");
-        Scanner s = new Scanner(System.in);
-        Date d = null;
-        while (d == null)
-        {
-            try{
-                d = date.parse(s.nextLine());
-            } catch (ParseException e) {
-                System.out.println("Wrong date format! Try again.");
-            }
-        }
-        System.out.println("OK");
-        System.out.println("Next day is: ");
+    public static void main(String[] args) throws Exception{
+        doTask();
+        //2013-02-25T18:25+0300
+    }
+    static void doTask() throws ParseException
+    {
+        LocalDate date = LocalDate.parse("2011-12-03T10:15:30+01:00", DateTimeFormatter.ISO_OFFSET_DATE_TIME);
+
+        System.out.println(date.format());
+    }
+    static void test() throws ParseException {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MM-dd");
+        Date date = dateFormat.parse("01-02");
+        System.out.println(dateFormat.format(date));
     }
 }
