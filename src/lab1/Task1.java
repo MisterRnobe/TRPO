2cb423834461458db211147281ce73e5597497f5
MisterRnobe <mr.rnobe@gmail.com>
2018-02-21 15:18:05 +0300

    Tasks are done

--- src/lab1/Task1.java
+++ src/lab1/Task1.java
@@ -1,45 +1,55 @@
 package lab1;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.*;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Task1 {
     public static void main(String[] args){
-
-        SimpleDateFormat date = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
-        Scanner s = new Scanner(System.in);
-        Date d = null;
-        while (d == null)
-        {
-            try{
-                d = date.parse(s.nextLine());
-            } catch (ParseException e) {
-                System.out.println("Wrong date format! Try again.");
-            }
-        }
-        System.out.println("OK");
-        System.out.println("Parsed: "+date.format(d));
-        Calendar cal = Calendar.getInstance();
-        cal.setTime(d);
+        String input = "2018-01-31T13:00:00+09:00";
+        ZonedDateTime date = ZonedDateTime.parse(input,
+                DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssZ"));
         //Next day
-        Calendar cal1 = (Calendar) cal.clone();
-        cal1.add(Calendar.DAY_OF_YEAR, 1);
-        System.out.println("Next day is: "+date.format(cal1.getTime()));
-        //Beginning and end of the week
-        Calendar cal2 = (Calendar) cal.clone();
-        int dayOfWeek = cal2.get(Calendar.DAY_OF_WEEK) - 1;
-        if (dayOfWeek == 0)
-            dayOfWeek = 7;
-        cal2.add(Calendar.DAY_OF_YEAR, -(dayOfWeek - 1));
-        System.out.println("The specified week stars at: "+date.format(cal2.getTime()));
-        cal2.add(Calendar.DAY_OF_YEAR, 6);
-        System.out.println("The specified week ends at: "+date.format(cal2.getTime()));
+        System.out.println("Next day is "+date.plusDays(1));
+        //Start and end of this week
+        int dayOfWeek = date.getDayOfWeek().getValue();
+        System.out.println("Week starts with: " + date.minusDays(dayOfWeek - 1)+"\nWeek ends at: "+date.plusDays(7 - dayOfWeek));
         //Next month
-        Calendar cal3 = (Calendar) cal.clone();
-        cal3.add(Calendar.MONTH, 1);
-        cal3.add(Calendar.DAY_OF_YEAR, -(cal3.get(Calendar.DAY_OF_MONTH) - 1));
-        System.out.println("Next month start at: "+date.format(cal3.getTime()));
+        System.out.println("Next month is: "+date.minusDays(date.getDayOfMonth() - 1).plusMonths(1));
+
+//        SimpleDateFormat date = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
+//        Scanner s = new Scanner(System.in);
+//        Date d = null;
+//        while (d == null)
+//        {
+//            try{
+//                d = date.parse(s.nextLine());
+//            } catch (ParseException e) {
+//                System.out.println("Wrong date format! Try again.");
+//            }
+//        }
+//        System.out.println("OK");
+//        System.out.println("Parsed: "+date.format(d));
+//        Calendar cal = Calendar.getInstance();
+//        cal.setTime(d);
+//        //Next day
+//        Calendar cal1 = (Calendar) cal.clone();
+//        cal1.add(Calendar.DAY_OF_YEAR, 1);
+//        System.out.println("Next day is: "+date.format(cal1.getTime()));
+//        //Beginning and end of the week
+//        Calendar cal2 = (Calendar) cal.clone();
+//        int dayOfWeek = cal2.get(Calendar.DAY_OF_WEEK) - 1;
+//        if (dayOfWeek == 0)
+//            dayOfWeek = 7;
+//        cal2.add(Calendar.DAY_OF_YEAR, -(dayOfWeek - 1));
+//        System.out.println("The specified week stars at: "+date.format(cal2.getTime()));
+//        cal2.add(Calendar.DAY_OF_YEAR, 6);
+//        System.out.println("The specified week ends at: "+date.format(cal2.getTime()));
+//
+//        //Next month
+//        Calendar cal3 = (Calendar) cal.clone();
+//        cal3.add(Calendar.MONTH, 1);
+//        cal3.add(Calendar.DAY_OF_YEAR, -(cal3.get(Calendar.DAY_OF_MONTH) - 1));
+//        System.out.println("Next month start at: "+date.format(cal3.getTime()));
 
     }
 }
---
+++ src/lab1/Task2.java
@@ -0,0 +1,48 @@
+package lab1;
+
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Task2 {
+    private static final String dateRegex = "[0-9]{4}-((1[02]|0[13578])-(0[1-9]|[12][0-9]|3[0-1])|(0[469]|11)-(0[1-9]|1[0-9]|2[0-9]|30)|02-(0[1-9]|1[0-9]|2[0-8]))";
+    public static void main(String[] args)
+    {
+        System.out.println("Enter:\n\t1 - to check date\n\t2 - to check date and time\n\t3 - to check mail");
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        System.out.println("Enter string: ");
+        String text = scanner.nextLine();
+        switch (n) {
+            case 1: checkDate(text); break;
+            case 2: checkDateAndTime(text); break;
+            case 3: checkMail(text); break;
+        }
+        scanner.close();
+    }
+    private static void checkDate(String text)
+    {
+        Pattern pattern = Pattern.compile(dateRegex);
+        Matcher matcher = pattern.matcher(text);
+        while(matcher.find())
+            System.out.println("Found: "+matcher.group(0));
+    }
+    private static void checkDateAndTime(String text)
+    {
+        String dateTimeRegex = dateRegex +
+                "T([01][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])";
+        Pattern pattern = Pattern.compile(dateTimeRegex);
+        Matcher matcher = pattern.matcher(text);
+        while (matcher.find())
+            System.out.println("Found: "+matcher.group(0));
+    }
+    private static void checkMail(String text)
+    {
+        String regex = "[-\\w.]+@[A-z0-9][-A-z0-9]+\\.+[A-z]{2,4}";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+        while (matcher.find())
+            System.out.println("Found: " + matcher.group(0));
+    }
+}
